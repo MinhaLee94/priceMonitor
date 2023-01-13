cf759f23bec7a10bd2392a9b3423489cac74a265
Minha Lee <shjune1994@gmail.com>
2023-01-13 14:18:28 -0500

    Add project files.

---
+++ priceMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace priceMonitor {
+    class Program {
+        static void Main(string[] args) {
+            string temp = "http://www.staples.com/product_2710522";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(temp);
+
+            System.Diagnostics.Debug.WriteLine(htmlDoc.DocumentNode.OuterHtml);
+            Console.WriteLine(htmlDoc.Text);
+        }
+    }
+}
