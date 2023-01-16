a809df08852aba975f7b81a7e4ab42fa23b218bf
Minha Lee <shjune1994@gmail.com>
2023-01-16 15:23:50 -0500

    Built excel reader and search item list generator function

--- priceMonitor/Program.cs
+++ priceMonitor/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
 using HtmlAgilityPack;
+using ExcelDataReader;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Collections;
+
+// transfer itemList to each store
+// create url-formatting function and xlsx file generating function
 
 namespace priceMonitor {
     class Program {
-        static void Main(string[] args) {
-            string temp = "http://www.staples.com/product_2710522";
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument htmlDoc = web.Load(temp);
+        private static ArrayList itemsToSearch = new ArrayList();
+        private static readonly string directory = "C:\\Users\\john\\Desktop\\SKU Status";
+        private static string[] fileEntries = Directory.GetFiles(directory);
 
-            System.Diagnostics.Debug.WriteLine(htmlDoc.DocumentNode.OuterHtml);
-            Console.WriteLine(htmlDoc.Text);
+        private static ArrayList generateItemListToSearch() {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            DataTableCollection tablecollection;
+            DataTable dt;
+            ArrayList itemList = new ArrayList();
+
+            foreach (string fileName in fileEntries) {
+                if (fileName != @"C:\Users\john\Desktop\SKU Status\Walmart Report 010923.xlsx") continue; // for testing
+
+                using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read)) {
+                    using (var reader = ExcelReaderFactory.CreateReader(stream)) {
+                        var result = reader.AsDataSet(new ExcelDataSetConfiguration() {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration() {
+                                EmptyColumnNamePrefix = "Column",
+                                UseHeaderRow = true
+                            }
+                        });
+
+                        tablecollection = result.Tables;
+
+                        foreach (DataTable datatable in tablecollection) {
+                            if (datatable.TableName.Contains("Discontinued") || datatable.TableName.Contains("No SKUs") || datatable.TableName == "susepnd") continue;
+
+                            foreach (DataRow item in datatable.Rows) {
+                                if (item["Status"].ToString() != "ON SITE") continue;
+
+                                Dictionary<string, string> itemInfo = new Dictionary<string, string>() {
+                                    {"Joy SKU", item["Joy SKU"].ToString()},
+                                    {"Reseller SKU", item["Reseller SKU"].ToString()},
+                                    {"Description", item["Description"].ToString()},
+                                };
+
+                                itemList.Add(itemInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            return itemList;
+        }
+
+        static void Main(string[] args) {
+
+            itemsToSearch = generateItemListToSearch();
+
+            /*string url = "https://www.walmart.com/ip/666786665";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(url);*/
+
+            //System.Diagnostics.Debug.WriteLine(htmlDoc.DocumentNode.OuterHtml);
+            //Console.WriteLine(htmlDoc.Text);
         }
     }
 }
---
+++ priceMonitor/Walmart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace priceMonitor {
+    class Walmart {
+        private static readonly string baseUrl = "https://www.walmart.com/ip/";
+
+        public string formatUrl(string resellerSku, string description) {
+            string result = "";
+
+            return result;
+        }
+
+        // url formatting
+    }
+}
